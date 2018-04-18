e8481107cd9e5ee2bf90050425c82b605428c0df
Milan Dierick <milan.dierick@gmail.com>
2018-04-18 10:04:24 +0200

    WIP: Working on FileManager class

--- MazeRunner_core/Utilities/FileManager.cpp
+++ MazeRunner_core/Utilities/FileManager.cpp
@@ -1,1 +1,32 @@
 ﻿#include "FileManager.h"
+#include <string>
+
+std::vector<std::string> FileManager::ReadAllLinesFromFile(const std::string& path)
+{
+	inputStream.open(path, std::ios::in);
+
+	if (inputStream.is_open())
+	{
+		std::vector<std::string> returnValues;
+		std::string line;
+
+		while (std::getline(inputStream, line))
+			returnValues.push_back(line);
+
+		inputStream.close();
+		return returnValues;
+	}
+	
+	throw;
+}
+
+void FileManager::WriteToFile(const std::string &path, std::vector<std::string> data)
+{
+	outputStream.open(path, std::ios::out);
+
+	if (outputStream.is_open())
+		for (const auto &value : data)
+			outputStream << value << std::endl;
+	else
+		throw;
+}
--- MazeRunner_core/Utilities/FileManager.h
+++ MazeRunner_core/Utilities/FileManager.h
@@ -1,7 +1,14 @@
 ﻿#pragma once
+#include <fstream>
+#include <vector>
 
 class FileManager
 {
 public:
-	
+	static std::vector<std::string> ReadAllLinesFromFile(const std::string& path);
+	static void WriteToFile(const std::string& path, std::vector<std::string> data);
+
+private:
+	static std::ofstream outputStream;
+	static std::ifstream inputStream;
 };
