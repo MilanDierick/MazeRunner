46917c90a438dd7c91b76e64c27777c10ac8379b
Milan Dierick <milan.dierick@gmail.com>
2018-04-20 15:32:24 +0200

    WIP: prepared test environment

--- MazeRunner_core/MazeRunner_core/FileManager.cpp
+++ MazeRunner_core/MazeRunner_core/FileManager.cpp
@@ -1,4 +1,5 @@
-﻿#include "FileManager.h"
+﻿/*
+#include "FileManager.h"
 #include <string>
 
 std::vector<std::string> FileManager::ReadAllLinesFromFile(const std::string& path)
@@ -28,5 +29,39 @@ void FileManager::WriteToFile(const std::string &path, std::vector<std::string> 
 		for (const auto &value : data)
 			outputStream << value << std::endl;
 	else
-		throw;
+		throw; // TODO: remove this hack, this class shouldn't throw when it is not indicated to throw.
+}
+*/
+
+#include "FileManager.h"
+#include <string>
+
+std::vector<std::string> FileManager::ReadAllLinesFromFile(const std::string& path)
+{
+	inputStream.open(path, std::ios::in);
+
+	if (inputStream.is_open())
+	{
+		std::vector<std::string> returnValues;
+		std::string line;
+
+		while (std::getline(inputStream, line))
+			returnValues.push_back(line);
+
+		inputStream.close();
+		return returnValues;
+	}
+
+	throw;
+}
+
+void FileManager::WriteToFile(const std::string &path, std::vector<std::string> data)
+{
+	outputStream.open(path, std::ios::out);
+
+	if (outputStream.is_open())
+		for (const auto &value : data)
+			outputStream << value << std::endl;
+	else
+		throw; // TODO: remove this hack, this class shouldn't throw when it is not indicated to throw.
 }
