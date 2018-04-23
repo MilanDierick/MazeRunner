f8d0ec265d66c3da49638a8248599aaa119380da
Milan Dierick <milan.dierick@gmail.com>
2018-04-23 11:17:57 +0200

    REF: Removed testing of FileManager

--- MazeRunner_core/MazeRunner_core/FileManager.h
+++ MazeRunner_core/MazeRunner_core/FileManager.h
@@ -1,21 +1,4 @@
-﻿/*
-#pragma once
-#include <fstream>
-#include <vector>
-
-class FileManager
-{
-public:
-	static std::vector<std::string> ReadAllLinesFromFile(const std::string& path);
-	static void WriteToFile(const std::string& path, std::vector<std::string> data);
-
-private:
-	static std::ofstream outputStream;
-	static std::ifstream inputStream;
-};
-*/
-
-#pragma once
+﻿#pragma once
 #include <fstream>
 #include <vector>
 
