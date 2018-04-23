9b7d0d0e8f5102d7bc02f498f176c1b23f1f647d
KennethVanPoucke <kenneth.vanpoucke@gmail.com>
2018-04-23 16:30:14 +0200

    ADD: idprovider

--- MazeRunner_core/Utilities/IdProvider.cpp
+++ MazeRunner_core/Utilities/IdProvider.cpp
@@ -1,1 +1,21 @@
 ﻿#include "IdProvider.h"
+
+IdProvider::IdProvider()
+{
+	this->id = 0;
+	count = 0;
+}
+IdProvider::IdProvider(const int id, const static int count)
+{
+	this->id = id;
+	IdProvider::count = count;
+}
+
+std::string IdProvider::IdToString(const int id)
+{
+	auto idstring = std::to_string(id);
+	return idstring;
+}
+
+int IdProvider::count;
+
--- MazeRunner_core/Utilities/IdProvider.h
+++ MazeRunner_core/Utilities/IdProvider.h
@@ -1,7 +1,12 @@
 ﻿#pragma once
+#include <string>
 
 class IdProvider
 {
 public:
-	
+	int id;
+	static int count;
+	IdProvider();
+	IdProvider(int id,static int count);
+	static std::string IdToString(const int id);
 };
